15cc030a973b6f38ab5524e54ecf84aaea366787
HCI <ark10806>
2021-09-30 02:29:03 +0900

    exprname edited

--- 1Phase/mmdetection/configs/_base_/waste_runtime_4VGA.py
+++ 1Phase/mmdetection/configs/_base_/waste_runtime_4VGA.py
@@ -1,4 +1,4 @@
-expr_name = 'hrnC_Retro_ms4121024_18'
+expr_name = 'hrnC_Retro_ms4121024_32'
 
 dist_params = dict(backend='nccl')
 
@@ -23,4 +23,4 @@ workflow = [('train', 1)]
 # evaluation = dict(save_best='bbox_mAP', metric=['bbox'])
 runner = dict(type='EpochBasedRunner', max_epochs=50)
 work_dir = './work_dirs/' + expr_name
-gpu_ids = range(0, 1)
+gpu_ids = range(0, 4)
