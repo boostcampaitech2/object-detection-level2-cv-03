c73a5b67cbd19fae906aad26e302bcfd0481b678
root <ark10806@gmail.com>
2021-09-29 19:26:49 +0000

    dataAug

--- 1Phase/EDA/dataAug.py
+++ 1Phase/EDA/dataAug.py
@@ -2,7 +2,7 @@ import json
 import os
 from glob import glob
 from tqdm import tqdm
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 from random import randint
 from collections import Counter
@@ -18,12 +18,16 @@ import copy
 imsize = 1024
 margin = 30
 n_class = 10
+dstroot = './aug'
+bboxroot = './bbox'
+
 base = '/opt/ml/detection/dataset'
 train_path = os.path.join(base, 'train_v1.json')
 
 train_json = json.load(open(train_path, 'r'))
 
 images = train_json['images']
+new_images = []
 annots = train_json['annotations']
 init_annot_len = len(annots)
 cls_annots = [[] for _ in range(n_class)]
@@ -95,7 +99,7 @@ for imgj in images:
 
 
 cls_panel = get_cls_panel()
-def cutmix2bg(src_img_id, dst_img_id, ann_id, n_patch):
+def cutmix2bg(src_img_id, dst_img_id, fname, ann_id, n_patch):
     is_patched = 0
     src = Image.open(os.path.join(base, img_dic[src_img_id]))
     bg = getbg(src_img_id)
@@ -107,30 +111,74 @@ def cutmix2bg(src_img_id, dst_img_id, ann_id, n_patch):
             is_patched += 1
             ann_id += 1
             src.paste(patch, coord[:2])
+            x,y,w,h = coord
             annots.append({
                 'image_id': dst_img_id,
                 'category_id': patch_cls, 
                 'area': coord[2]*coord[3], 
-                'bbox': coord, 'iscrowd': 0, 
+                'bbox': [float(x), float(y), float(w), float(h)], 
+                'iscrowd': 0, 
                 'id': ann_id})
             x,y,w,h = coord
             bg[x:x+w, y:y+h] = 1
     if is_patched:
         ##########DST로 바꿔야 함
-        src.save(f'./aug/{src_img_id}_{is_patched}.jpg')
-    return ann_id
+        src.save(os.path.join(dstroot, f'{fname}.jpg'))
+    print(is_patched)
+    return ann_id, is_patched
 
 def main():
-    n_aug = 100
+    n_aug = 10
     n_patch_max = 10
     ## json 저장해야함
     dst_img_id = len(images)
     ann_id = len(annots)
+    fname = 5000
     for _ in tqdm(range(n_aug)):
         src_img_id = img_ids[randint(0, len(images)-1)]
         n_patch = randint(0, n_patch_max)
-        ann_id = cutmix2bg(src_img_id, dst_img_id, ann_id, n_patch)
+        ann_id, is_patched = cutmix2bg(src_img_id, dst_img_id, fname, ann_id, n_patch)
+        if is_patched:
+            new_images.append(
+                {'width': 1024, 'height': 1024,
+                    'file_name': f'aug/{fname}.jpg',
+                    # 'file_name': f'train/{fname}.jpg',
+                    'license': 0, 'flickr_url': None, 'coco_url': None, 'date_captured': '2021-01-10 16:30:39', 
+                    'id': dst_img_id})
+        fname += 1
         dst_img_id += 1
+    
+    train_json['images'] = images + new_images
+    train_json['annotations'] = annots
+    with open(os.path.join(dstroot, 'train_aug.json'), 'w') as f:
+        json.dump(train_json, f)
+
+
+
+font = ImageFont.truetype('./ubuntu.regular.ttf', 15)
+color_group = {'General trash': 'orange', 'Paper': 'white', 'Paper pack': 'ivory', 'Metal': 'dimgrey', 'Glass': 'dodgerBlue', 'Plastic': 'darkolivegreen', 'Styrofoam': 'khaki', 'Plastic bag': 'Teal', 'Battery': 'lime', 'Clothing': 'fuchsia'}
+cates = []
+for v in train_json['categories']:
+    cates.append(v['name'])
+
+def drawbox(draw, cate, pt1, pt2):
+    draw.rectangle((pt1, pt2), outline=color_group[cate], width=3)
+    backrec = (pt1[0]+90, pt1[1]+15)
+    # draw.rectangle((pt1, backrec), outline=(0,0,0), fill=True)
+    draw.text(pt1, cate, (255,255,255), font=font, backrec=True)
+
+def draw():
+    images = new_images
+    for image in tqdm(images):
+        img = Image.open(os.path.join('./', image['file_name']))
+        draw = ImageDraw.Draw(img)
+        for ann in annots:
+            if ann['image_id'] == image['id']:
+                x,y,w,h = ann['bbox']
+                pt1, pt2 = (x,y), (x+w, y+h)
+                drawbox(draw, cates[ann['category_id']], pt1, pt2)
+                img.save(os.path.join(bboxroot, image['file_name'].split('/')[-1]))
 
 if __name__ == '__main__':
     main()
+    draw()
