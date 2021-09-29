78c9ac2c8a1e11d6fa139ac9145e6cf36b4e988a
root <ark10806@gmail.com>
2021-09-29 13:46:00 +0000

    hi

---
+++ 1Phase/mmdetection3/configs/_base_/models/waste_hrnet_16.py
@@ -0,0 +1,23 @@
+_base_ = [
+    # '../datasets/waste_dataset.py',
+    # '../schedules/waste_schedule.py', '../waste_runtime.py',
+    # '/opt/ml/detection/object-detection-level2-cv-03/1Phase/mmdetection/configs/hrnet/cascade_rcnn_hrnetv2p_w18_20e_coco.py'
+    './waste_hrnet_32.py'
+]
+
+
+model = dict(
+    backbone=dict(
+        extra=dict(
+            stage2=dict(num_channels=(18, 36)),
+            stage3=dict(num_channels=(18, 36, 72)),
+            stage4=dict(num_channels=(18, 36, 72, 144))),
+        init_cfg=dict(
+            type='Pretrained', checkpoint='open-mmlab://msra/hrnetv2_w18')),
+    neck=dict(type='HRFPN', in_channels=[18, 36, 72, 144], out_channels=256))
+
+
+
+data = dict(
+    samples_per_gpu = 16
+)
