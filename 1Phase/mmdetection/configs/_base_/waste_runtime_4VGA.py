7512b9ed0aa9b049cb7ecce8d003c756ed390ec2
root <ark10806@gmail.com>
2021-09-30 19:35:16 +0000

    hi

--- 1Phase/mmdetection/configs/_base_/waste_runtime_4VGA.py
+++ 1Phase/mmdetection/configs/_base_/waste_runtime_4VGA.py
@@ -22,6 +22,6 @@ load_from = None
 resume_from = None
 workflow = [('train', 1)]
 # evaluation = dict(save_best='bbox_mAP', metric=['bbox'])
-runner = dict(type='EpochBasedRunner', max_epochs=50)
+runner = dict(type='EpochBasedRunner', max_epochs=40)
 work_dir = './work_dirs/' + expr_name
 gpu_ids = range(0, 4)
