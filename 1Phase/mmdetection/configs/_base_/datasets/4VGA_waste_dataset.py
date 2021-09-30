a20c727e238bb4efe35fa5f500257aaa711e6e64
root <ark10806@gmail.com>
2021-09-30 19:39:28 +0000

    hi

--- 1Phase/mmdetection/configs/_base_/datasets/4VGA_waste_dataset.py
+++ 1Phase/mmdetection/configs/_base_/datasets/4VGA_waste_dataset.py
@@ -5,7 +5,7 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 
 
-imsize = 1024
+imsize = 1024 - 32*4
 
 multi_scale_dict = []
 for i in range(416,imsize+1,32):
